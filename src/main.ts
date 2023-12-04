df0b23ef17e7a35a3671de8b1a6f84917422e3e6
mariacabriles29 <mecp2992@gmail.com>
2023-12-04 10:28:48 -0400

    feat: Post receive and validate data

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   //configuro una accion global
   app.setGlobalPrefix('api/v2');
+
+  //configuracion global de los pipes para usar decoradores
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
