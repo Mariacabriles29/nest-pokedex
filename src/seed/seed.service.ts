fb5c0155b3c32b2271f73fab2cae92983914ce14
mariacabriles29 <mecp2992@gmail.com>
2023-12-05 10:46:46 -0400

    feat: Insert multiple records simultaneously

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -13,15 +13,23 @@ export class SeedService {
     private readonly pokemonModel: Model<Pokemon>,
   ) {}
   async executeSeed() {
+    //este deleteMany va eliminar lo que ya esta cada vez que ejecute el seed para no tener duplicados
+    await this.pokemonModel.deleteMany({});
     const { data } = await this.axios.get<PokeResponse>(
-      'https://pokeapi.co/api/v2/pokemon?limit=10',
+      'https://pokeapi.co/api/v2/pokemon?limit=650',
     );
     //para barrer la data y hacer insercion de datos por lote
-    data.results.forEach(async ({ name, url }) => {
+    const pokemonToInsert: { name: string; no: number }[] = [];
+    data.results.forEach(({ name, url }) => {
       const segments = url.split('/');
       const no: number = +segments[segments.length - 2];
-      const pokemon = await this.pokemonModel.create({ name, no });
+      //const pokemon = await this.pokemonModel.create({ name, no });
+
+      //para insertar mutiples registros
+      pokemonToInsert.push({ name, no });
     });
+    //inserta todos los pokemones por name y numero
+    await this.pokemonModel.insertMany(pokemonToInsert);
     return 'Seed Executed';
   }
 }
