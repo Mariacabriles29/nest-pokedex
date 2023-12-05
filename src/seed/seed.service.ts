77daae9a95bba4dd45bb76960e9dd0dcb12d2c27
mariacabriles29 <mecp2992@gmail.com>
2023-12-05 11:22:21 -0400

    feat: custom provider for axios requests

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,21 +1,22 @@
 import { Injectable } from '@nestjs/common';
-import axios, { AxiosInstance } from 'axios';
+
 import { PokeResponse } from './interfaces/poke-response.interface';
 import { InjectModel } from '@nestjs/mongoose';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { Model } from 'mongoose';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 
 @Injectable()
 export class SeedService {
-  private readonly axios: AxiosInstance = axios;
   constructor(
     @InjectModel(Pokemon.name)
     private readonly pokemonModel: Model<Pokemon>,
+    private readonly http: AxiosAdapter,
   ) {}
   async executeSeed() {
     //este deleteMany va eliminar lo que ya esta cada vez que ejecute el seed para no tener duplicados
     await this.pokemonModel.deleteMany({});
-    const { data } = await this.axios.get<PokeResponse>(
+    const data = await this.http.get<PokeResponse>(
       'https://pokeapi.co/api/v2/pokemon?limit=650',
     );
     //para barrer la data y hacer insercion de datos por lote
