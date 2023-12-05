0a55db77f12e0c4a76dbc032c4df75315dccb588
mariacabriles29 <mecp2992@gmail.com>
2023-12-05 10:19:11 -0400

    feat: batch data insertion

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,20 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class SeedService {
   private readonly axios: AxiosInstance = axios;
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
   async executeSeed() {
     const { data } = await this.axios.get<PokeResponse>(
       'https://pokeapi.co/api/v2/pokemon?limit=10',
     );
-    //para barrer la data
-    data.results.forEach(({ name, url }) => {
+    //para barrer la data y hacer insercion de datos por lote
+    data.results.forEach(async ({ name, url }) => {
       const segments = url.split('/');
       const no: number = +segments[segments.length - 2];
-      console.log({ name, no });
+      const pokemon = await this.pokemonModel.create({ name, no });
     });
-    return data.results;
+    return 'Seed Executed';
   }
 }
